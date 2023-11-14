4a2e310745030478dde6a2578411d3fb39874695
estefano97 <kaliestefano@gmail.com>
2023-11-14 14:53:13 -0500

    first commit

---
+++ api.crypto.training/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using api.crypto.training.Requests;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.crypto.training.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("login")]
+        public IActionResult Login([FromBody]LoginDTO model)
+        {
+            try
+            {
+                if(model.Password == "12345" && model.Email == "dianita@gmail.com")
+                {
+                    return Ok("logeado con exito!");
+                }
+                return BadRequest("correo o contrasenia invalidos!");
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
